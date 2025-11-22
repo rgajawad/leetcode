5dd472a8d4fad68f5889143f525634e360dbd2a7
rgajawad <rgajawad@gmail.com>
2025-11-22 01:40:06 -0600

    Time: 9 ms (46.47%) | Memory: 11.9 MB (21.51%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -16,7 +16,7 @@ public:
     bool hasCycle(ListNode *head) {
         if(!head || !head->next) return false;
         ListNode* slow = head;
-        ListNode* fast = head->next;
+        ListNode* fast = head;
         while(fast && fast->next){
             slow = slow->next;
             fast = fast->next->next;
