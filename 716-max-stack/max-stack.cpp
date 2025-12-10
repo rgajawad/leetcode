cf2ece44e7a7dc643f043cbe7e72076a23602567
rgajawad <rgajawad@gmail.com>
2025-12-10 15:00:26 -0600

    Time: 103 ms (64.60%) | Memory: 148.9 MB (71.32%) - LeetSync

--- 716-max-stack/max-stack.cpp
+++ 716-max-stack/max-stack.cpp
@@ -1,88 +1,40 @@
-struct Node{
-    Node* next;
-    Node* prev;
-    int val;
-    Node(int val){
-       this->val = val; 
-       this->next = NULL;
-       this->prev = NULL;
-    }
-};
-
-struct compare{
-    bool operator()(Node* a, Node* b) const{
-        return a->val < b->val;
-    }
-};
-
 class MaxStack {
-
 private:
-    Node* head;
-    Node* tail;
-    map<int, vector<Node*>> mp;
+    set<pair<int, int>> stack;
+    set<pair<int, int>> values;
+    int cnt;
 
 public:
     MaxStack() {
-        this->head = new Node(-1);
-        this->tail = new Node(-1);
-        head->next = tail;
-        tail->prev = head;
+        cnt = 0;
     }
     
     void push(int x) {
-        Node* node = new Node(x);
-        Node* prev_tail = tail->prev;
-
-        prev_tail->next = node;
-        node->prev = prev_tail;
-
-        node->next = tail;
-        tail->prev = node;
-        mp[x].push_back(node);
+        stack.insert({cnt, x});
+        values.insert({x, cnt});
+        cnt++;
     }
     
     int pop() {
-        Node* prev_tail = tail->prev;
-        int val = prev_tail->val;
-
-        Node* node_prev = prev_tail->prev;
-        Node* node_next = prev_tail->next;
-
-        node_prev->next = node_next;
-        node_next->prev = node_prev;
-
-        mp[val].pop_back();
-        if(mp[val].size() == 0) mp.erase(val);
-
-        delete prev_tail;
-        return val;
+        auto p = *stack.rbegin();
+        stack.erase(p);
+        values.erase({p.second, p.first});
+        return p.second;
     }
     
     int top() {
-        Node* prev_tail = tail->prev;
-        return prev_tail->val; 
+        return stack.rbegin()->second;
     }
     
     int peekMax() {
-        return mp.rbegin()->first;
+        return values.rbegin()->first;
     }
     
     int popMax() {
-        int maxVal = mp.rbegin()->first;
-        Node* maxNode = mp[maxVal].back();
-
-        mp[maxVal].pop_back();
-        if(mp[maxVal].size() == 0) mp.erase(maxVal);
-        
-        Node* node_prev = maxNode->prev;
-        Node* node_next = maxNode->next;
-
-        node_prev->next = node_next;
-        node_next->prev = node_prev;
-        
-        delete maxNode; 
-        return maxVal;
+        auto p = *values.rbegin();
+        values.erase(p);
+        stack.erase({p.second, p.first});
+        return p.first;
     }
 };
 
