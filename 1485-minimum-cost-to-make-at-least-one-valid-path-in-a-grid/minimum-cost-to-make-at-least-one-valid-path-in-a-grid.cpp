bc2affe0f79d5749a7ee47339ab91f14298a7f76
rgajawad <rgajawad@gmail.com>
2026-08-23 13:44:53 -0500

    Time: 37 ms (31.16%) | Memory: 34.5 MB (12.39%) - LeetSync

--- 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
+++ 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
@@ -5,24 +5,6 @@ private:
     return x >= 0 && y >= 0 && x < m && y < n;
   }
 
-    bool edgeCost(int val, int dx, int dy){
-        if(val == 1 && dx == 0 && dy == 1){
-            return 0;
-        }
-        if(val == 2 && dx == 0 && dy == -1){
-            return 0;
-        }
-
-        if(val == 3 && dx == 1 && dy == 0){
-            return 0;
-        }
-
-        if(val == 4 && dx == -1 && dy == 0){
-            return 0;
-        }
-        return 1;
-
-    }
 public:
     int minCost(vector<vector<int>>& grid) {
         int m = grid.size();
@@ -30,6 +12,10 @@ public:
 
         vector<vector<int>> dist(m, vector<int>(n, INT_MAX));
         dist[0][0] = 0;
+
+        auto isValid = [m, n](int r, int c){
+            return r >= 0 && c >= 0 && r < m && c < n;
+        };
         
         deque<pair<int,int>> dq;
         dq.push_front({0,0});
@@ -41,8 +27,8 @@ public:
                 auto [dx,dy] = dirs[i];
                 int nr, nc;
                 nr = x + dx, nc = y + dy;
-                int cost = edgeCost(grid[x][y], dx, dy);
-                if(isValid(nr,nc, m, n)){
+                int cost = i+1 == grid[x][y] ? 0 : 1;
+                if(isValid(nr,nc)){
                     if(dist[x][y] + cost < dist[nr][nc]){
                         dist[nr][nc] = dist[x][y] + cost;
                         if(cost == 0){
