180d848c88d3b8514bafce3b7cf7b30e0a3f3f97
rgajawad <rgajawad@gmail.com>
2026-09-11 12:57:17 -0500

    Time: 85 ms (18.18%) | Memory: 71.5 MB (5.07%) - LeetSync

--- 729-my-calendar-i/my-calendar-i.cpp
+++ 729-my-calendar-i/my-calendar-i.cpp
@@ -1,23 +1,47 @@
 class MyCalendar {
+
+struct Node{
+    bool lazy = false;
+    Node* left = nullptr;
+    Node* right = nullptr;
+};
+
+Node* root;
+const int MAX = 1e9;
 private:
     map<int,int> mp;
+
+    bool query(Node* node, int start, int end, int l, int r){
+        if(!node || l >= end || r <= start) return false;
+        if(node->lazy) return true;
+        int mid = start + (end-start)/2;
+        return query(node->left, start, mid, l,r) || query(node->right, mid, end, l, r);
+    }
+
+    void update(Node* node, int start, int end, int l, int r){
+        if(l >= end || r <= start)  return;
+        if(l <= start && end <= r){
+            node->lazy = true;
+            return;
+        }
+        int mid = start + (end - start)/2;
+        if(!node->left) node->left = new Node();
+        if(!node->right) node->right = new Node();
+        update(node->left, start, mid, l , r);
+        update(node->right, mid, end, l , r);
+
+    }
+
 public:
     MyCalendar() {
-
+        root = new Node();
     }
 
     // [10, 20] [15, 25]
     
     bool book(int startTime, int endTime) {
-        auto iter = mp.lower_bound(startTime);
-        if(iter != mp.begin()){
-            auto prevIter = prev(iter);
-            if(startTime < prevIter->second && endTime > prevIter->first) return false;
-        }
-        if(iter!= mp.end()){
-            if(startTime < iter->second && endTime > iter->first) return false;
-        }
-        mp[startTime] = endTime;
+        if(query(root, 0, MAX, startTime, endTime)) return false;
+        update(root, 0,MAX, startTime,endTime);
         return true;
     }
 };
